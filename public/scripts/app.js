2c05af90f72a98a22fe4a6ed81b250f1ce2094e9
Maliha Hossain <malu.hoss@gmail.com>
2019-03-28 19:57:42 +0000

    working on add and delete button for order page

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,13 +1,40 @@
-$(() => {
-  $.ajax({
-    method: "GET",
-    url: "/api/users"
-  }).done((items) => {
-    for(item of items) {
-      $("<div>").text(item.name).appendTo($("body"));
-      $("<div>").text(item.Price).appendTo($("body"));
-    }  
+$(document).ready(function() {
+  // $.ajax({
+  //   method: "GET",
+  //   url: "/api/users"
+  // }).done((items) => {
+  //   for(item of items) {
+  //     $("<div>").text(item.name).appendTo($("body"));
+  //     $("<div>").text(item.Price).appendTo($("body"));
+  //   }
+  // });
+
+  $("form div").prepend('<i class="fas fa-minus-circle"></i>');
+  $("form div").append('<i class="fas fa-plus-circle"></i>');
+
+  $(".fas fa-minus-circle").on("click", function() {
+
+    var $button = $(this);
+    var oldValue = $button.parent().find("input").val();
+    var newVal = parseFloat(oldValue) + 1;
+    $button.parent().find("input").val(newVal);
+    });
+
+    $(".fas fa-plus-circle").on("click", function() {
+     // Don't allow decrementing below zero
+      if (oldValue > 0) {
+        var newVal = parseFloat(oldValue) - 1;
+      } else {
+        newVal = 0;
+      }
+    $button.parent().find("input").val(newVal);
+
   });
+
+  // $(".add").on("click", function(event) {
+  //   let input = 0
+  //   $(".quantity").on()
+  // })
 });
 
 
