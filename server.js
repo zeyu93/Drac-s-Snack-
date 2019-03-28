268b77347049d519cddca3e619915aaf49de1028
Zeyu <haozai_03@hotmail.com>
2019-03-28 18:22:33 +0000

    added some post routes

--- server.js
+++ server.js
@@ -51,6 +51,10 @@ app.get("/register", (req, res) => {
   res.render("register");
 });
 
+app.post("/register", (req,res)=> {
+  res.redirect("/")
+})
+
 app.get("/order", (req, res) => {
   res.render("order");
 });
