f7142c4564dafac2fa1176e7a6e7c87b1a9ee9c8
Zeyu <haozai_03@hotmail.com>
2019-03-28 22:14:15 +0000

    added twilio function

--- public/scripts/send_sms.js
+++ public/scripts/send_sms.js
@@ -4,20 +4,13 @@
 const accountSid = 'AC6fcfd1e98597d109e3cf0fa3bd99c85d';
 const authToken = '6f4862ebdff859830d9ff1db6d1ed382';
 const client = require('twilio')(accountSid, authToken);
-const http = require('http');
+
 const express = require('express');
-const MessagingResponse = require('twilio').twiml.MessagingResponse;
+
 
 const app = express();
 
-app.post('/sms', (req, res) => {
-  const twiml = new MessagingResponse();
 
-  twiml.message('The Robots are coming! Head for the hills!');
-
-  res.writeHead(200, {'Content-Type': 'text/xml'});
-  res.end(twiml.toString());
-});
 
 http.createServer(app).listen(1337, () => {
   console.log('Express server listening on port 1337');
