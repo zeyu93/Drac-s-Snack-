679594e3f600dff298dc5353bc7db6ee9f5f2cac
PeterBiskupski <pete.biskupski@gmail.com>
2019-03-29 00:31:28 +0000

    dropped description column in 'items' table

--- public/scripts/send_sms.js
+++ public/scripts/send_sms.js
@@ -1,6 +1,7 @@
 // Download the helper library from https://www.twilio.com/docs/node/install
 // Your Account Sid and Auth Token from twilio.com/console
 // DANGER! This is insecure. See http://twil.io/secure
+
 const accountSid = 'AC6fcfd1e98597d109e3cf0fa3bd99c85d';
 const authToken = '6f4862ebdff859830d9ff1db6d1ed382';
 const client = require('twilio')(accountSid, authToken);
