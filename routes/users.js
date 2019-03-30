28979cec144df9f0720886e26977413c1db7257e
Zeyu <haozai_03@hotmail.com>
2019-03-30 22:14:51 +0000

    small change

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,13 @@
 "use strict";
+const accountSid = 'ACb821873d0587d85c246d76ace12fadfa';
+const authToken = '95275827719a1f480ac9ad58e6425fbe';
 
 const express = require('express');
 const router  = express.Router();
+const MessagingResponse = require('twilio').twiml.MessagingResponse;
+const http = require('http');
+
+const client = require('twilio')(accountSid, authToken);
 
 module.exports = (knex) => {
 
@@ -36,13 +42,25 @@ module.exports = (knex) => {
 
 
   // ('INSERT INTO orders_items (item_id, item_quantity, created_at) values (CURRENT_TIMESTAMP) RETURNING id')
+  function send_sms(params){
+    client.messages
+      .create({
+     body: 'Your order has been received, we are working hard to process your order!',
+     from: '+16474924614',
+     to: '+14168097087'
+     })
+    .then(message => console.log(message.sid));
+
+  }
 
   router.post("/checkout", (req,res) => {
+    console.log('body of request:', req.body)
     knex("users")
     .insert({name: req.body.name, phone: req.body.phone_no})
     .then((results) => {
       let templateVars = {name: req.body.name, phone: req.body.phone_no};
       console.log(templateVars)
+      send_sms(req.body.phone_no)
       res.redirect("/confirmed")
     });
   });
